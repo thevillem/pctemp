d0a8d3fd7b07db8e34c4d03e1aad09277734f67a
Administrator <admin@home.lan>
2020-01-02 12:27:28 -0500

    Adding pre-commit hooks.

--- temp.py
+++ temp.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
 
-import wmi
-import json
 import datetime
+import json
+
 import requests
+import wmi
 
 # TODO:
 # 1. Command line parameter for sending data.
 # 2. Changing send_temp to send_msg ?
 # 2a. Add timestamp to JSON data before sending.
+# 3. Add a logger instead of print for messaging.
 
 
 def send_temp(stats_list):
@@ -23,14 +25,14 @@ def get_temp():
     stats_list = []
     for sensor in temperature_infos:
         json_data = {
-                    "time" : str(datetime.datetime.utcnow().isoformat()),
-                    "Identifier" : sensor.Identifier,
-                    "Name" : sensor.Name,
-                    "SensorType" : sensor.SensorType,
-                    "Value" : sensor.Value,
-                    "Max" : sensor.Max,
-                    "Min" : sensor.Min
-                }
+            "time": str(datetime.datetime.utcnow().isoformat()),
+            "Identifier": sensor.Identifier,
+            "Name": sensor.Name,
+            "SensorType": sensor.SensorType,
+            "Value": sensor.Value,
+            "Max": sensor.Max,
+            "Min": sensor.Min,
+        }
         json_data = json.dumps(json_data)
         stats_list.append(json_data)
 
@@ -44,7 +46,7 @@ def main():
         print("OpenHardwareMonitor is running!")
     else:
         print("OpenHardwareMonitor is not running, exiting.")
-        
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
